d9067fdc10116a980c75e776355dc48f3611316f
MUAMMER AKCA <muammerakca@MUAMMERs-MacBook-Air.local>
2022-11-24 14:15:37 +0300

    Project Created and added ML model, imageView, resultLabel and new PHPicker

---
+++ MLDeneme2/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  MLDeneme2
+//
+//  Created by MUAMMER AKCA on 24.11.2022.
+//
+
+import UIKit
+import Photos
+import PhotosUI
+
+class ViewController: UIViewController, PHPickerViewControllerDelegate {
+    
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        
+        picker.dismiss(animated: true)
+        
+        results[0].itemProvider.loadObject(ofClass: UIImage.self) { (reading, error) in
+            guard let image = reading as? UIImage, error == nil else {return}
+            self.imageView.image = image
+        }
+        
+        
+    }
+    
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(gorselDegistir))
+        
+        imageView.addGestureRecognizer(gesture)
+        
+    }
+    
+    @objc func gorselDegistir(){
+        
+        
+        var configuration = PHPickerConfiguration(photoLibrary: .shared())
+        
+        configuration.selectionLimit = 1
+        configuration.filter = PHPickerFilter.images
+        let vc = PHPickerViewController(configuration: configuration)
+        vc.delegate = self
+        present(vc, animated: true)
+        
+    }
+    
+    
+    
+}
+
